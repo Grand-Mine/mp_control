1739e682e92f8751e1681d6ddb97085c7f40371d
Maksym Prymierov <primerovmax@gmail.com>
2020-09-04 01:56:25 +0200

    Add config file
    Add config file with information about project
    
    Signed-off-by: Maksym Prymierov <primerovmax@gmail.com>

--- mp_control.py
+++ mp_control.py
@@ -3,18 +3,37 @@
 import os
 from os import path
 from os.path import expanduser
+from configparser import ConfigParser
 
+config_object = ConfigParser()
 home_dir = expanduser("~")
 config_dir = home_dir + "/.mp_control"
 
 def check_configs():
     return path.exists(config_dir)
 
-def create_configs():
+def create_configs(c):
     os.mkdir(config_dir)
     with open(config_dir + "/config", "w"): pass
 
+    print("Enter the name of project: ")
+    project_name = input()
+
+    print("Enter the name of project directory (the directory shoult exist in the home directory): ")
+    project_dir = input()
+
+    c["PROJECT_INFO"] = {
+        "name": project_name,
+        "dir": home_dir + "/" + project_dir
+    }
+
+    with open(config_dir + "/config", "w") as conf:
+        c.write(conf)
+
+
 print("MP Minecraft control panel starting...")
 if not check_configs():
-    create_configs()
+    create_configs(config_object)
     print("Configs have been created in " + config_dir + " directory")
+
+config_object.read(config_dir + "/config")
